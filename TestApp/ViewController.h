409a3e728d4d221089356021098fc81c00c31134
Blake Lucchesi <blake@mattermark.com>
2015-06-10 16:21:34 -0700

    Initial Commit

---
+++ TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Blake Lucchesi on 6/10/15.
+//  Copyright (c) 2015 Mattermark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by Blake Lucchesi on 6/10/15.
+//  Copyright (c) 2015 Mattermark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
